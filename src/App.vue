c756d3c40d69479b66181877bbd0255471a458a5
whyang9701 <whyang9701@gmail.com>
2019-12-14 11:54:27 +0000

    sinalR test

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <div id="app">
+    
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      
     </div>
     <router-view />
   </div>
 </template>
 
+
+<script>
+
+</script>
 <style lang="scss">
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
@@ -30,3 +36,4 @@
   }
 }
 </style>
+
