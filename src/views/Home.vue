dc17b0be43c6ec918d723df20789c8206ad47e4e
Chih Hui Kao <kchforwork@gmail.com>
2019-12-14 20:01:12 +0800

    add landing page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <LandingPage />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
+import LandingPage from "@/components/LandingPage.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    LandingPage
+    // HelloWorld
   }
 };
 </script>
